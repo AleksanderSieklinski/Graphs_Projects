6e86356f0be297ffd2ef1c2a2a1cdcd7a8cf4787
skolanko222@gmail.com <skolanko222@gmail.com>
2024-03-17 15:40:44 +0100

    k-regular random with bugs2

--- lab2/zad5/k_regular.py
+++ lab2/zad5/k_regular.py
@@ -1,18 +1,18 @@
-from adjacencyIncidenceMatrixes import MyGraph
+from graph_lib import MyGraph
 
-# DRAFT
-
-def generate_k_regular_graph(n, k):
+def generate_k_regular_graph(n, k): # n - ilość wierzchołków, k - stopień
     if n <= k:
         print("n must be greater than k")
         return None
-    if k % 2 != 0 and n % 2 != 0:
-        print("n must be even")
+    if k*n % 2 != 0:
+        print("k*n must be even")
         return None
     
     # ciąg stopni wierzchołków
     degree_sequence = [[i, k] for i in range(n)] # i - wierzchołek, k - stopień wierzchołka
     adj_list = [ [] for i in degree_sequence]
+    print(degree_sequence)
+    
 
     while True:
         # sprawdzamy czy w degree_sequence stopnie wierzchołków są równe k
@@ -25,16 +25,20 @@ def generate_k_regular_graph(n, k):
             b = degree_sequence[i][0]
             adj_list[a].append(b)    
             adj_list[b].append(a)        
-
-        degree_sequence[0][1] = 0
         
+        degree_sequence[0][1] = 0
         degree_sequence.sort(reverse=True, key=lambda x: x[1])
-
-    return MyGraph(adj_list, 1)
+        print(degree_sequence)
+    g = MyGraph(adj_list,1)
+    g.saveFigure("k_regular_graph.png")
+    g.randomize_graph(100) # tu się psuje
+    g.saveFigure("k_regular_graph_random.png")
+    return g
 
 if __name__ == "__main__":
-	n_value = 5  # Ilość wierzchołków
-	k_value = 4  # Stopień
-
+	n_value = 4  # Ilość wierzchołków
+	k_value = 2  # Stopień
+    
 	generated_graph = generate_k_regular_graph(n_value, k_value)
-	generated_graph.saveFigure("k_regular_graph.png")
+	if generated_graph is not None:
+		generated_graph.saveFigure("k_regular_graph_random.png")
