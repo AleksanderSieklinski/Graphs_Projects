a1a8767f27b2142b252ffed7da9e6864a40ef90c
skolanko222 <skolanko222@gmail.com.com>
2024-03-30 10:27:29 +0100

    k-regular graphs (lab2zad5) fix
    added random k-regular graph construction, changed showGraph to be able to save graph to file, if filename provided

--- graph_lib.py
+++ graph_lib.py
@@ -228,7 +228,7 @@ class MyGraph:
     
     
     #Draws graph
-    def showGraph(self):
+    def showGraph(self, filename = None):
         #get edges from adjacencyList
         edges = []
         for i in range(len(self.adjacencyList)):
@@ -245,7 +245,11 @@ class MyGraph:
         #set layout and draw graph with specified parameters
         pos = nx.circular_layout(G)
         nx.draw(G, pos, with_labels=True, node_size=700, node_color="skyblue", font_size=8, font_color="black", font_weight="bold", edge_color="gray", linewidths=0.5)
-        plt.show()
+        if filename != None:
+            plt.savefig(filename)
+            plt.close()
+        else:
+            plt.show()
 
 
     
@@ -374,6 +378,38 @@ class MyGraph:
             index += 1
             res = np.count_nonzero(colored == index)
         return  colored, index - 1
+    
+    
+    def generateKRegularGraph(n, k): # n - number of vertices, k - degree
+        if n <= k:
+            raise Exception("n must be greater than k")
+        if k * n % 2 != 0:
+            raise Exception("k * n must be even")
+
+        
+        # degree sequence
+        degreeSequence = [[i, k] for i in range(n)] # i - vertex, k - degree of vertex
+        adjList = [[] for i in degreeSequence]
+        # print(degreeSequence)
+        
+
+        while True:
+            # check if all degrees in degreeSequence are equal to k
+            if all(i[1] == 0 for i in degreeSequence):
+                break
+            for i in range(1, degreeSequence[0][1] + 1):
+                degreeSequence[i][1] -= 1
+                # add an edge between vertices a and b
+                a = degreeSequence[0][0]
+                b = degreeSequence[i][0]
+                adjList[a].append(b)    
+                adjList[b].append(a)        
+            
+            degreeSequence[0][1] = 0
+            degreeSequence.sort(reverse=True, key=lambda x: x[1])
+        g = MyGraph(adjList, 1)
+        # g.randomizeGraph(1)
+        return g
 
 
 if __name__ == "__main__":
--- lab2_5.py
+++ lab2_5.py
@@ -2,7 +2,7 @@ from graph_lib import MyGraph
 
 if __name__ == "__main__":
     nValue = 4  # number of vertices
-    kValue = 2  # degree
+    kValue = 3  # degree
     
     generatedGraph = MyGraph.generateKRegularGraph(nValue, kValue)
     if generatedGraph is not None:
